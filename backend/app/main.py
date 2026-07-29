e53bb1506d250c95c55b7ca4c5f54aec10f10e29
Adhishreegupta <guptaadhishree22@gmail.com>
2026-07-29 22:06:57 +0530

    fix cors for vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,15 +12,19 @@ from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi import _rate_limit_exceeded_handler
 
+
 # Create limiter
 limiter = Limiter(key_func=get_remote_address)
+
 
 app = FastAPI(
     title="CopyCart AI API"
 )
 
+
 # Register limiter
 app.state.limiter = limiter
+
 
 # Rate limit handler
 app.add_exception_handler(
@@ -28,29 +32,41 @@ app.add_exception_handler(
     _rate_limit_exceeded_handler
 )
 
+
 # Middleware
 app.add_middleware(SlowAPIMiddleware)
 
+
+# CORS Configuration
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+
+        # Vercel production URL
+        "https://himshakti-ai-product-description-generator-adhi13.vercel.app",
+
+        # Vercel git deployment URL
+        "https://himshakti-ai-product-description-generator-git-main-adhi13.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Static folder
+
+# Static folder for uploaded images
 app.mount(
     "/uploads",
     StaticFiles(directory="uploads"),
     name="uploads"
 )
 
+
 # Routers
 app.include_router(router)
 app.include_router(auth_router)
+
 
 @app.get("/")
 def home():
