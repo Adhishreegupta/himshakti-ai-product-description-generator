eee219c139bbff79fe8f08ee33904779a31e4199
Adhishreegupta <guptaadhishree22@gmail.com>
2026-07-29 20:20:24 +0530

    Fix Firebase import for Vercel

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,8 +5,8 @@ import axios from "axios";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import hero from "../assets/hero-bg1.jpg";
-import { signInWithPopup } from "Firebase/auth";
-import { auth, googleProvider } from "../Firebase";
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "../firebase";
 import { toast } from "react-toastify";
 function Login() {
 
